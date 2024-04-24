07e6554c110ba203f58491d5507d411853f1da6e
mringel-code <76100579+mringel-code@users.noreply.github.com>
2024-04-24 11:33:55 +0200

    Update server.py

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
     with Configurator() as config:
         config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='Hallo')
+        config.add_view(hello_world, route_name='hello')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
     server.serve_forever()
