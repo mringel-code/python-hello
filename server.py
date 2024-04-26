194c1b1d6cdef753f56b18dd6b6bcad5225c0f6b
Maik <>
2024-04-26 14:39:32 +0000

    calculator

--- server.py
+++ server.py
@@ -1,13 +1,34 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+application = Flask(__name__)
+application.config.from_object(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    message = ''
-    if request.method == 'POST':
-        message = 'Button clicked!'
-    return render_template('index.html', message=message)
+
+@application.route('/')
+def welcome():
+    return render_template('form.html')
+
+
+@application.route('/result', methods=['POST'])
+def result():
+    var_1 = request.form.get("var_1", type=int)
+    var_2 = request.form.get("var_2", type=int)
+    operation = request.form.get("operation")
+    if var_1 is None or var_2 is None:
+        result = 'Lỗi'
+    else:
+        if(operation == 'Cộng'):
+            result = var_1 + var_2
+        elif(operation == 'Trừ'):
+            result = var_1 - var_2
+        elif(operation == 'Nhân'):
+            result = var_1 * var_2
+        elif(operation == 'Chia'):
+            result = var_1 / var_2
+        else:
+            result = 'Dữ liệu chưa được nhập'
+    entry = result
+    return render_template('result.html', entry=entry)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    application.run(host='0.0.0.0',port=8080)
